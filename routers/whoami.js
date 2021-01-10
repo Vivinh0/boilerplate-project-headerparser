19f789b0df23c6706cec5e392f4d4fbf2e487bce
Viviño <Vivinh0@users.noreply.github.com>
2021-01-10 15:58:20 +0100

    Add /api/whoami endpoint logic

--- routers/whoami.js
+++ routers/whoami.js
@@ -3,4 +3,16 @@
 const express = require("express");
 const router = express.Router();
 
-module.exports = router.get("", (req, res, next) => res.json({ msg: "Ok" }));
+module.exports = router.get("", (req, res, next) => {
+  // res.json({ msg: "Ok" });
+  const clientIpAddress = req.ip;
+  const clientAcceptLang = req.headers["accept-language"] || "en-US,en;q=0.5";
+  const clientUserAget = req.headers["user-agent"];
+  const response = {
+    ipaddress: clientIpAddress,
+    language: clientAcceptLang,
+    software: clientUserAget,
+  };
+  // console.log(response);
+  res.json(response);
+});
